5fe881cb2e3a9ae0f76703802f1bcfc94d017f4d
DG-Thiru <thiruthu.dg@gmail.com>
2024-08-09 17:20:48 +0530

    Source File

---
+++ ProductMangement/ProductMaster.aspx
@@ -0,0 +1,88 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ProductMaster.aspx.cs" Inherits="ProductMangement.ProductMaster" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <table>
+                <tr>
+                    <td>ProductName  
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtProductName" runat="server"></asp:TextBox>
+                    </td>
+                    <td>Size</td>
+                    <td>
+                        <asp:DropDownList ID="ddlSize" runat="server">
+                            <asp:ListItem Text="--Select--" Value="0"></asp:ListItem>
+                            <asp:ListItem Text="S" Value="S"></asp:ListItem>
+                            <asp:ListItem Text="M" Value="M"></asp:ListItem>
+                            <asp:ListItem Text="L" Value="L"></asp:ListItem>
+                        </asp:DropDownList>
+                    </td>
+                    <td>Price
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtPrice" runat="server"></asp:TextBox>
+                    </td>
+
+                    <td>Catagory</td>
+                    <td>
+                        <asp:DropDownList ID="ddlCategory" runat="server">
+                            <asp:ListItem Text="--Select--" Value="0"></asp:ListItem>
+                            <asp:ListItem Text="Standard" Value="Standard"></asp:ListItem>
+                            <asp:ListItem Text="Premium" Value="Premium"></asp:ListItem>
+                            <asp:ListItem Text="Economy" Value="Economy"></asp:ListItem>
+                        </asp:DropDownList>
+                    </td>
+
+                    <td>Date
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtDate" runat="server"></asp:TextBox>
+                    </td>
+                    <td>
+
+                        <asp:Calendar ID="dtMFG" runat="server"  OnSelectionChanged="dtMFG_SelectionChanged" ></asp:Calendar>
+                    </td>
+                </tr>
+            </table>
+            <table>
+                <tr>
+                    <td>
+                        
+
+                        <asp:Button ID="btnSearch" runat="server" Text="Search" OnClick="btnSearch_Click"></asp:Button>
+
+                    </td>
+                    <td>
+
+
+                        <asp:Button ID="btnClear" runat="server" Text="Clear" OnClick="btnClear_Click"></asp:Button>
+
+                    </td>
+                </tr>
+            </table>
+          
+                    <asp:GridView ID="grdProduct" runat="server" AutoGenerateColumns="false">
+                        <Columns>
+                            <asp:BoundField HeaderText="ProductID" DataField="ProductID" />
+                            <asp:BoundField HeaderText="ProductName" DataField="ProductName" />
+                            <asp:BoundField HeaderText="Size" DataField="Size" />
+                            <asp:BoundField HeaderText="Price" DataField="Price" />
+                            <asp:BoundField HeaderText="MfgDate" DataField="MfgDate" />
+                            <asp:BoundField HeaderText="Category" DataField="Category" />
+
+                        </Columns>
+                    </asp:GridView>
+               
+        </div>
+    </form>
+</body>
+</html>
---
+++ ProductMangement/ProductMaster.aspx.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+namespace ProductMangement
+{
+    public partial class ProductMaster : System.Web.UI.Page
+    {
+        string strcon = ConfigurationManager.AppSettings["ConnectionString"].ToString();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+               
+                Resetsearch();
+                
+                SearchDetails();
+            }
+        }
+
+       
+        public void Resetsearch()
+        {
+            txtProductName.Text = string.Empty;
+            txtPrice.Text = string.Empty;
+            txtDate.Text = string.Empty;
+            ddlCategory.SelectedValue = "0";
+            ddlSize.SelectedValue = "0";
+            SearchDetails();
+        }
+        public void SearchDetails()
+        {
+            try
+            {
+
+
+                string ProductName, Size, Catagory, Mfgdate, Price;
+
+                if (txtProductName.Text == "")
+                {
+                    ProductName = "";
+                }
+                else
+                {
+                    ProductName = txtProductName.Text;
+                }
+                if (ddlSize.SelectedValue == "0")
+                {
+                    Size = "";
+                }
+                else
+                {
+                    Size = ddlSize.SelectedValue;
+                }
+                if (txtPrice.Text == "")
+                {
+                    Price = "";
+                }
+                else
+                {
+                    Price = txtPrice.Text;
+                }
+                if (ddlCategory.SelectedValue == "0")
+                {
+                    Catagory = "";
+                }
+                else
+                {
+                    Catagory = ddlCategory.SelectedValue;
+                }
+                if (txtDate.Text == "")
+                {
+                    Mfgdate = "";
+                }
+                else
+                {
+                    Mfgdate = txtDate.Text;
+                }
+                SqlConnection con = new SqlConnection(strcon);
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "[ProductSearch]";
+
+                cmd.Parameters.Add("@ProductName", SqlDbType.VarChar).Value = ProductName;
+                cmd.Parameters.Add("@Size", SqlDbType.VarChar).Value = Size;
+                if (string.IsNullOrWhiteSpace(Price))
+                {
+                    cmd.Parameters.Add("@Price", SqlDbType.Float).Value = DBNull.Value;
+                }
+                else
+                {
+                    cmd.Parameters.Add("@Price", SqlDbType.Float).Value = Convert.ToDouble(Price);
+                }
+                if (string.IsNullOrWhiteSpace(Mfgdate))
+                {
+                    cmd.Parameters.Add("@MfgDate", SqlDbType.VarChar).Value = DBNull.Value;
+                }
+                else
+                {
+                    cmd.Parameters.Add("@MfgDate", SqlDbType.VarChar).Value = Convert.ToDateTime(Mfgdate).ToString("yyyy-MM-dd");
+                }
+
+
+                cmd.Parameters.Add("@Category", SqlDbType.VarChar).Value = Catagory;
+
+                cmd.Connection = con;
+                con.Open();
+                grdProduct.EmptyDataText = "No Records Found";
+
+                SqlDataReader dr = cmd.ExecuteReader();
+                grdProduct.DataSource = dr;
+
+
+                grdProduct.DataBind();
+                con.Close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(String.Concat(e.Message,e.StackTrace));
+            }
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            SearchDetails();
+        }
+
+        protected void dtMFG_SelectionChanged(object sender, EventArgs e)
+        {
+            txtDate.Text = dtMFG.SelectedDate.ToString();
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            Resetsearch();
+        }
+    }
+    
+}
